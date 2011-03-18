4233c217ccd9fd0e6e89d94baa0f5b225733d95d
Pierre Vittet <piervit@pvittet.com>
2011-03-18 16:45:58 +0100

    Ajout du listener Collision

---
+++ include/ListenerCollision.h
@@ -0,0 +1,85 @@
+/*!
+ *  \file  ListenerCollision.h
+ *  \brief Ce fichier contient la déclaration de la classe ListenerCollision. C'est la classe gérant les collisions. On utilise la librairie OgreBullet
+ */
+#ifndef __LISTENER_COLLISION_H__
+#define __LISTENER_COLLISION_H__
+
+#include <Ogre.h>
+#include "OgreBulletCollisionsPreRequisites.h"
+#include "OgreBulletCollisionsRay.h"
+#include "OgreBulletDynamicsPreRequisites.h"
+#include "OgreBulletDynamicsRigidBody.h"
+#include "Shapes/OgreBulletCollisionsSphereShape.h"
+
+#include "ClassRoot.h"
+#include "ListenerFrame.h"
+
+class ObjectRoot;
+
+/*!
+ * \class ListenerCollision
+ * \brief Classe permettant de gérer les événements de collision
+ */
+class ListenerCollision : public ClassRoot
+{
+	private :
+		/*!
+		 * \brief Définit le monde pour OgreBullet
+		 */
+		OgreBulletDynamics::DynamicsWorld * mWorld;
+		/*!
+		 * \brief Trace de débug pour OgreBullet
+		 */
+		OgreBulletCollisions::DebugDrawer * debugDrawer;
+
+	public :
+		/*!
+		 * \brief Constructeur
+		 * \param listenerFrame Pointeur sur le controlleur de frame
+		 */
+		ListenerCollision();
+		/*!
+		 * \brief Constructeur
+		 */
+		~ListenerCollision();
+
+		/*!
+		 *  \brief Retourne une référence sur l'instance du singleton
+		 */		
+		static void createSingleton();
+		/*!
+		 *  \brief Retourne un pointeur sur l'instance du singleton
+		 */
+		static ListenerCollision * getSingletonPtr();
+		/*!
+		 *  \brief Retourne une référence sur l'instance du singleton
+		 */		
+		static ListenerCollision & getSingleton();
+        /*!
+         * \brief Detruit le singleton
+         */
+        static void destroySingleton();
+		
+		
+
+
+
+		/*!
+		 * \brief Met à jour les collision
+		 * \param evt Evenement du controlleur de frame
+		 */
+		void updateCollision(const Ogre::FrameEvent &evt);
+
+		/*!
+		 * \brief [Getter] Pointeur sur le monde d'ogreBullet
+		 * \return Retourne un pointeur sur le monde d'ogreBullet
+		 */
+		OgreBulletDynamics::DynamicsWorld * getWorld()
+		{
+			return mWorld;
+		}
+};
+
+#endif //__LISTENER_COLLISION_H__
+
---
+++ src/ListenerCollision.cpp
@@ -0,0 +1,78 @@
+#include "ListenerCollision.h"
+
+
+
+ListenerCollision * ListenerCollision::_instance = NULL;
+
+void ListenerCollision::createSingleton()
+{
+	if (_instance == NULL)
+	{
+		if(Application::getSingletonPtr()->getInputManager() != NULL)
+		{
+			_instance = new ListenerCollision();
+		}
+	}
+}
+
+ListenerCollision * ListenerCollision::getSingletonPtr()
+{
+	if (_instance == NULL)
+	{
+		if(Application::getSingletonPtr()->getInputManager() != NULL)
+		{
+			_instance = new ListenerCollision();
+		}
+	}
+	return _instance;
+}
+
+ListenerCollision & ListenerCollision::getSingleton()
+{
+	if (_instance == NULL)
+	{
+		if(Application::getSingletonPtr()->getInputManager() != NULL)
+		{
+			_instance = new ListenerCollision();
+		}
+	}
+	return *_instance;
+}
+
+void ListenerCollision::destroySingleton()
+{
+    if(_instance != NULL)
+    {
+        delete _instance;
+    }
+}
+
+ListenerCollision::ListenerCollision()
+{	
+    //Start Bullet
+    mWorld = new OgreBulletDynamics::DynamicsWorld(GestSceneManager::getSceneManager(), Ogre::AxisAlignedBox(Ogre::Vector3 (-1, -1, -1), Ogre::Vector3 (1,  1,  1)), Ogre::Vector3(0,0,0));
+    //add Debug info display tool
+    debugDrawer = new OgreBulletCollisions::DebugDrawer();
+    debugDrawer->setDrawWireframe(true);   // we want to see the Bullet containers
+    mWorld->setDebugDrawer(debugDrawer);
+    mWorld->setShowDebugShapes(true);      // enable it if you want to see the Bullet containers
+    Ogre::SceneNode *node_debugDrawer = GestSceneManager::getSceneManager()->getRootSceneNode()->createChildSceneNode("debugDrawer", Ogre::Vector3::ZERO);
+    node_debugDrawer->attachObject(static_cast <Ogre::SimpleRenderable *> (debugDrawer));
+    mWorld->getBulletDynamicsWorld()->clearForces();
+
+    ListenerFrame::getSingletonPtr()->signalFrameStarted.add(&ListenerCollision::updateCollision, this);
+    //listenerFrame->signalFrameEnded.add(&ListenerCollision::updateCollision, this);
+
+}
+
+ListenerCollision::~ListenerCollision()
+{
+    delete mWorld->getDebugDrawer();
+    mWorld->setDebugDrawer(0);
+    delete mWorld;
+}
+
+void ListenerCollision::updateCollision(const Ogre::FrameEvent &evt)
+{
+
+}
