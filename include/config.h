625f2eeda444ee3c4a96a170113b1f761dfa24ba
Pierre Vittet <piervit@pvittet.com>
2011-04-12 17:49:34 +0200

    Debut du menus CEGUI

---
+++ include/config.h
@@ -0,0 +1,25 @@
+/*!
+ **  \file  config.h
+ **  \brief This file contains the declaration of the constants for the configuration of the projects
+ **/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+	/*!
+	 **  \brief Nom du projet
+	 **/
+	#define PROJECT_NAME "BriquettePhysique"
+	/*!
+	 **  \brief Version du projet
+	 **/
+	#define PROJECT_VERSION "1.0"
+	/*!
+	 **  \brief Version complète du projet
+	 **/
+	#define PROJECT_VERSION_FULL "1.0.0"
+	/*!
+	 **  \brief Nom complet du projet
+	 **/
+	#define PROJECT_FULLNAME PROJECT_NAME" "PROJECT_VERSION_FULL
+
+#endif
